02069272360d09b6000211347adba1e3a23b7bd4
nillkizz <alx.n.smith@gmail.com>
2022-05-06 03:24:13 +0300

    Fix double slash on shortcodes url. + Changes

--- wp-neon-constructor/shortcodes/wp-neon-constructor/template.php
+++ wp-neon-constructor/shortcodes/wp-neon-constructor/template.php
@@ -1,2 +1,10 @@
-<div class="123">123</div>
-<?php
+<div id="wp-neon-constructor-app" class="wnca" x-data="wnca">
+  <div class="form">
+    <div class="header">
+      <div class="tabs"></div>
+    </div>
+    <div class="content"></div>
+    <div class="footer"></div>
+  </div>
+  <div class="preview"></div>
+</div>
--- wp-neon-constructor/shortcodes/wp-neon-constructor/wp-neon-constructor.php
+++ wp-neon-constructor/shortcodes/wp-neon-constructor/wp-neon-constructor.php
@@ -4,8 +4,14 @@ namespace WPNeonConstructor;
 
 (function () use ($public_path) {
   global $wp_neon_constructor;
-  $wp_neon_constructor->enqueue_style(['name' => 'wp_neon_constructor-shortcode-style', 'path' => $public_path . 'style.css']);
-  $wp_neon_constructor->enqueue_script(['name' => 'wp_neon_constructor-shortcode-script', 'path' => $public_path . 'script.js']);
+  $wp_neon_constructor->enqueue_style([
+    'name' => 'wp_neon_constructor-shortcode-style',
+    'path' => $public_path . 'style.css'
+  ]);
+  $wp_neon_constructor->enqueue_script([
+    'name' => 'wp_neon_constructor-shortcode-script',
+    'path' => $public_path . 'script.js',
+  ]);
 
   include_once 'template.php';
 })();
