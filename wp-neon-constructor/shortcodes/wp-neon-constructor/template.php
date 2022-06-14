cbc49b19667b12bd744df811ac5468e33ea7bec0
nillkizz <alx.n.smith@gmail.com>
2022-06-14 16:01:54 +0300

    Form taken out from popup. Popup removed

--- wp-neon-constructor/shortcodes/wp-neon-constructor/template.php
+++ wp-neon-constructor/shortcodes/wp-neon-constructor/template.php
@@ -4,74 +4,76 @@ namespace WPNeonConstructor;
 ?>
 <div id="wnca" x-data="wnca" x-cloak>
   <div class="form">
-    <div class="header">
-      <ul class="tabs">
-        <template x-for="tab in tabs">
-          <li class="tab" :class="{active: activeTab==tab}" @hover="alert('hover')">
-            <button x-text="tab" @click="setTab(tab)"></button>
-          </li>
-        </template>
-      </ul>
-    </div>
-    <div class="content">
-      <div class="tab-content">
-        <div class="text" x-show="activeTab=='Text'" x-transition.in.duration.600>
-          <textarea x-model="val.text" placeholder="ENTER TEXT HERE
+    <div class="constructor-form">
+      <div class="header">
+        <ul class="tabs">
+          <template x-for="tab in tabs">
+            <li class="tab" :class="{active: activeTab==tab}" @hover="alert('hover')">
+              <button x-text="tab" @click="setTab(tab)"></button>
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="content">
+        <div class="tab-content">
+          <div class="text" x-show="activeTab=='Text'" x-transition.in.duration.600>
+            <textarea x-model="val.text" placeholder="ENTER TEXT HERE
 Press Enter/Return for a new line"></textarea>
-          <div class="buttons">
-            <button class="align-left" :class="{active: 'left' ==state.text.align}" @click="state.text.align = 'left'"><span class="dashicons dashicons-editor-alignleft"></span></button>
-            <button class="align-center" :class="{active: 'center' ==state.text.align}" @click="state.text.align = 'center'"><span class="dashicons dashicons-editor-aligncenter"></span></button>
-            <button class="align-right" :class="{active: 'right' == state.text.align}" @click="state.text.align = 'right'"><span class="dashicons dashicons-editor-alignright"></span></button>
+            <div class="buttons">
+              <button class="align-left" :class="{active: 'left' ==state.text.align}" @click="state.text.align = 'left'"><span class="dashicons dashicons-editor-alignleft"></span></button>
+              <button class="align-center" :class="{active: 'center' ==state.text.align}" @click="state.text.align = 'center'"><span class="dashicons dashicons-editor-aligncenter"></span></button>
+              <button class="align-right" :class="{active: 'right' == state.text.align}" @click="state.text.align = 'right'"><span class="dashicons dashicons-editor-alignright"></span></button>
+            </div>
           </div>
-        </div>
-        <div class="fonts" x-show="activeTab=='Font'" x-transition.in.duration.600>
-          <div class="title">Choose font</div>
-          <script type="json" x-ref="fonts_json">
-            <?php echo json_encode(array_map(function ($font) {
-              return ['name' => $font['name'], 'preview' => $font['preview']['sizes']['FontPreview'], 'link' => $font['file']['url']];
-            }, $settings['fonts'])); ?>
+          <div class="fonts" x-show="activeTab=='Font'" x-transition.in.duration.600>
+            <div class="title">Choose font</div>
+            <script type="json" x-ref="fonts_json">
+              <?php echo json_encode(array_map(function ($font) {
+                return ['name' => $font['name'], 'preview' => $font['preview']['sizes']['FontPreview'], 'link' => $font['file']['url']];
+              }, $settings['fonts'])); ?>
           </script>
-          <div class="fonts-items" x-data="{fonts: JSON.parse($refs.fonts_json.innerHTML)}" x-init="setFont(fonts[0])">
-            <template x-for="font in fonts">
-              <div class="font" @click="setFont(font)" :class="{active: font.name == val.font}">
-                <img :src="font.preview" :alt="font.name">
-              </div>
-            </template>
-          </div>
-        </div>
-        <div class="colors" x-show="activeTab=='Color'" x-transition.in.duration.600>
-          <script type="json" x-ref="colors_json">
-            <?php echo json_encode(array_map(function ($color) {
-              return [
-                'name' => $color['name'],
-                'color' => $color['color'],
-                'description' => $color['description'],
-                'preview' => $color['preview']['sizes']['NeonPreview'],
-                'detailPreview' => $color['detail-preview']['url']
-              ];
-            }, $settings['colors'])); ?>
-          </script>
-          <div class="color-items" x-data="{colors: JSON.parse($refs.colors_json.innerHTML)}" x-init="setColor(colors[0]);">
-            <template x-for="color in colors">
-              <div class="color" :class="{active: color.color == val.color}" @click="setColor(color)">
-                <div class="icon" :style="{color: color.color}">
-                  <i class="dashicons dashicons-lightbulb"></i>
-                  <i class="w dashicons dashicons-lightbulb"></i>
+            <div class="fonts-items" x-data="{fonts: JSON.parse($refs.fonts_json.innerHTML)}" x-init="setFont(fonts[0])">
+              <template x-for="font in fonts">
+                <div class="font" @click="setFont(font)" :class="{active: font.name == val.font}">
+                  <img :src="font.preview" :alt="font.name">
                 </div>
-                <div class="color-name" x-text="color.name"></div>
-              </div>
-            </template>
+              </template>
+            </div>
           </div>
-          <div class="color-preview">
-            <div class="desc" x-html="state.color.description" x-show="state.color.description"></div>
-            <img :src="state.color.preview" :alt="state.color.name" x-show="state.color.preview">
-            <div class="link" x-show="state.color.detailPreview"><strong>Detail preview:</strong> <a :href="state.color.detailPreview">Photos of <span x-text="state.color.name"></span></a></div>
+          <div class="colors" x-show="activeTab=='Color'" x-transition.in.duration.600>
+            <script type="json" x-ref="colors_json">
+              <?php echo json_encode(array_map(function ($color) {
+                return [
+                  'name' => $color['name'],
+                  'color' => $color['color'],
+                  'description' => $color['description'],
+                  'preview' => $color['preview']['sizes']['NeonPreview'],
+                  'detailPreview' => $color['detail-preview']['url']
+                ];
+              }, $settings['colors'])); ?>
+          </script>
+            <div class="color-items" x-data="{colors: JSON.parse($refs.colors_json.innerHTML)}" x-init="setColor(colors[0]);">
+              <template x-for="color in colors">
+                <div class="color" :class="{active: color.color == val.color}" @click="setColor(color)">
+                  <div class="icon" :style="{color: color.color}">
+                    <i class="dashicons dashicons-lightbulb"></i>
+                    <i class="w dashicons dashicons-lightbulb"></i>
+                  </div>
+                  <div class="color-name" x-text="color.name"></div>
+                </div>
+              </template>
+            </div>
+            <div class="color-preview">
+              <div class="desc" x-html="state.color.description" x-show="state.color.description"></div>
+              <img :src="state.color.preview" :alt="state.color.name" x-show="state.color.preview">
+              <div class="link" x-show="state.color.detailPreview"><strong>Detail preview:</strong> <a :href="state.color.detailPreview">Photos of <span x-text="state.color.name"></span></a></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="footer">
-      <button class="submit" @click="state.popup.toggle(true)"><?php echo $settings['submit_text']; ?></button>
+      <div class="footer">
+        <button @click="$refs.feedbackForm.scrollIntoView()"><?php echo $settings['submit_text']; ?></button>
+      </div>
     </div>
   </div>
   <div class="preview-wrapper">
@@ -83,21 +85,12 @@ Press Enter/Return for a new line"></textarea>
       <div class="text" x-bind="text"></div>
     </div>
   </div>
-  <template x-teleport="body">
-    <div id="wnca-form" class="popup form" x-show="state.popup.isOpen" x-transition.out.delay.300ms.opacity.duration.300 x-transition.enter.opacity.duration.300>
-      <div class="backdrop">
-        <div class="popup-body" @click.away="state.popup.toggle(false)" x-show="state.popup.isOpen" x-transition.duration.300>
-          <div class="popup-header">
-            <div class="close dashicons dashicons-no-alt" @click="state.popup.toggle(false)"></div>
-          </div>
-          <div class="popup-form">
-            <?php echo $settings['popup_form'] ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
 
+  <div id="wnca-form" class="form feedback-form" x-ref="feedbackForm">
+    <div class="form-body">
+      <?php echo $settings['popup_form'] ?>
+    </div>
+  </div>
   <template x-teleport="#wnca-form form">
     <div class="fields">
       <input type="text" x-model="val.text" name="text" hidden>
