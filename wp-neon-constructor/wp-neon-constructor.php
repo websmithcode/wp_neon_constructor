d42e5aba2aa6125fa26fff0321273cd7363d14b8
nillkizz <alx.n.smith@gmail.com>
2022-10-23 20:48:19 +0300

    FIX remove packing node_modules into plugin

--- wp-neon-constructor/wp-neon-constructor.php
+++ wp-neon-constructor/wp-neon-constructor.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Neon Constructor
  * Description: Template of plagin from dev kit
- * Version: 0.4.0
+ * Version: 0.4.1
  * Author: Alexander Smith
  * Author URI: https://t.me/alxndr_smith
  */
--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 rm wp-neon-constructor.zip
-zip -r wp-neon-constructor.zip wp-neon-constructor
+zip -r wp-neon-constructor.zip wp-neon-constructor -x "*/node_modules/**/*"
